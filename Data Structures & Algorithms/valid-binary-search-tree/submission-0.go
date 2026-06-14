f24aba4eaf582fe118d7a9a347347d86b76abca3
Iskander <83751807+Iskander-Dev@users.noreply.github.com>
2026-06-14 18:59:54 +0500

    Add: valid-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-0.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+    return isValid(root, math.MinInt, math.MaxInt)
+}
+
+func isValid(root *TreeNode, minValue, maxValue int) bool {
+	if root == nil {
+		return true
+	}
+	if minValue < root.Val &&
+	root.Val < maxValue && isValid(root.Left, minValue, root.Val) &&
+	isValid(root.Right, root.Val, maxValue) {
+		return true
+	} else {
+		return false
+	}
+}
